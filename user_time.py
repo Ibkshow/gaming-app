6d052afaa20ff1b29998510aaecdd513091563e1
ibukun <nathanjrk1245@gmail.com>
2025-11-18 09:04:27 -0800

    add user_time.py

---
+++ user_time.py
@@ -0,0 +1,16 @@
+# asking for user name
+user_name = input("what is your name?")
+print(f"hello, {user_name}!")
+
+# asking for favorite color
+color = input("what is your favorite color?")
+print(f"{color} is a nice color!")
+
+# combining two facts
+city = input("which city do you live in?")
+hobbies = input("what are your hobbies?")
+print(f"you live in {city} enjoys {hobbies}.")
+
+# asking for age
+age = int(input("how old are you?"))
+print(f"next year, you will be {age + 1}years old.")
